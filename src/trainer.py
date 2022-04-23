4c3cd227d55630fb5449d98bef9583f7ae39dbcc
Ehsan-Tavan <tavan.ehsan@gmail.com>
2022-04-23 21:38:28 +0430

    [add] add read_pickle

--- src/trainer.py
+++ src/trainer.py
@@ -17,7 +17,7 @@ from transformers import T5Tokenizer, MT5Tokenizer
 
 from configuration import BaseConfig
 from data_prepration import create_author_label, prepare_ap_data
-from data_loader import read_text
+from data_loader import read_text, read_pickle
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -29,32 +29,12 @@ if __name__ == "__main__":
     # create CSVLogger instance
     LOGGER = CSVLogger(save_dir=CONFIG.saved_model_path, name=CONFIG.model_name)
 
+    TRAIN_DATA = read_pickle(path=os.path.join(CONFIG.processed_data_dir,
+                                               CONFIG.train_data))
+    VAL_DATA = read_pickle(path=os.path.join(CONFIG.processed_data_dir,
+                                             CONFIG.val_data))
+    TEST_DATA = read_pickle(path=os.path.join(CONFIG.processed_data_dir,
+                                              CONFIG.test_data))
+
     # create LM Tokenizer instance
     # TOKENIZER = MT5Tokenizer.from_pretrained(CONFIG.language_model_tokenizer_path)
-
-    DATA = read_text(path=os.path.join(CONFIG.raw_data_dir, CONFIG.truth_data))
-    logging.debug("We have {} Author.".format(len(DATA)))
-
-    AUTHOR2LABEL = create_author_label(DATA)
-
-    DATA = prepare_ap_data(path=CONFIG.raw_data_dir, author2irony=AUTHOR2LABEL)
-
-    TRAIN_DATA, TEST_DATA = train_test_split(DATA,
-                                             test_size=0.3, random_state=1234)
-    VAL_DATA, TEST_DATA = train_test_split(TEST_DATA,
-                                           test_size=0.5, random_state=1234)
-
-    logging.debug("We have {} authors in train data.".format(len(TRAIN_DATA)))
-    logging.debug("We have {} authors in validation data.".format(len(VAL_DATA)))
-    logging.debug("We have {} authors in test data.".format(len(TEST_DATA)))
-
-    n_irony_author = 0
-    n_not_irony_author = 0
-    for data in TRAIN_DATA:
-        if data[1] == "I":
-            n_irony_author += 1
-        else:
-            n_not_irony_author += 1
-
-    logging.debug("We have {} irony authors and {} not irony  authors in TRAIN_DATA.".format(
-        n_irony_author, n_not_irony_author))
