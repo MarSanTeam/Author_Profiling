1d36128297f0a71ae13cb2473d89f5767e09890c
Ehsan-Tavan <tavan.ehsan@gmail.com>
2022-04-23 21:38:01 +0430

    [add] add read and write pickle function

--- src/data_loader/__init__.py
+++ src/data_loader/__init__.py
@@ -1,1 +1,4 @@
-from .data_reader import read_json, read_json5, read_text, read_xml
+from .data_reader import read_json, read_json5, read_text,\
+    read_xml, read_pickle
+
+from .data_writer import write_pickle
