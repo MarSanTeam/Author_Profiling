2e06f23b1229c1678f85bba03ba325334b6b8c38
Ehsan-Tavan <tavan.ehsan@gmail.com>
2022-04-23 21:37:36 +0430

    [add] add main

--- src/data_prepration/data_prepration.py
+++ src/data_prepration/data_prepration.py
@@ -22,3 +22,28 @@ def prepare_ap_data(path: str, author2irony: dict) -> list:
                     author_tweets.append(inch.text)
             data.append([author_tweets, author2irony[author_id]])
     return data
+
+
+# if name == "__main__":
+#     DATA = read_text(path=os.path.join(CONFIG.raw_data_dir, CONFIG.truth_data))
+#     logging.debug("We have {} Author.".format(len(DATA)))
+#
+#     AUTHOR2LABEL = create_author_label(DATA)
+#
+#     DATA = prepare_ap_data(path=CONFIG.raw_data_dir, author2irony=AUTHOR2LABEL)
+#
+#     TRAIN_DATA, TEST_DATA = train_test_split(DATA,
+#                                              test_size=0.3, random_state=1234)
+#     VAL_DATA, TEST_DATA = train_test_split(TEST_DATA,
+#                                            test_size=0.5, random_state=1234)
+#
+#     logging.debug("We have {} authors in train data.".format(len(TRAIN_DATA)))
+#     logging.debug("We have {} authors in validation data.".format(len(VAL_DATA)))
+#     logging.debug("We have {} authors in test data.".format(len(TEST_DATA)))
+#
+#     write_pickle(path=os.path.join(CONFIG.processed_data_dir, "train_data.pkl"),
+#                  data=TRAIN_DATA)
+#     write_pickle(path=os.path.join(CONFIG.processed_data_dir, "val_data.pkl"),
+#                  data=VAL_DATA)
+#     write_pickle(path=os.path.join(CONFIG.processed_data_dir, "test_data.pkl"),
+#                  data=TEST_DATA)
