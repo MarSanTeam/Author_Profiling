bed706499f79fead68f0379dbf40c5619ae675c6
Ehsan-Tavan <tavan.ehsan@gmail.com>
2022-04-27 00:21:26 +0430

    [add] add sbert

--- src/user_level_inferencer.py
+++ src/user_level_inferencer.py
@@ -10,14 +10,17 @@
 
 import os
 from sklearn import svm
+import numpy as np
 from transformers import BertModel, BertTokenizer
+from sentence_transformers import SentenceTransformer
 from sklearn.metrics import f1_score
 import logging
+import itertools
 
 from configuration import BaseConfig
 from data_prepration import prepare_ap_data, create_author_label
 from data_loader import read_text, read_pickle
-from utils import create_user_embedding
+from utils import create_user_embedding, create_user_embedding_sbert
 from indexer import Indexer
 
 logging.basicConfig(level=logging.DEBUG)
@@ -54,31 +57,36 @@ if __name__ == "__main__":
 
     TRAIN_TARGETS_CONVENTIONAL = [[target] for target in TRAIN_TARGETS]
     TRAIN_INDEXED_TARGET = TARGET_INDEXER.convert_samples_to_indexes(TRAIN_TARGETS_CONVENTIONAL)
+    TRAIN_INDEXED_TARGET = list(itertools.chain(*TRAIN_INDEXED_TARGET))
 
     logging.debug("Create train indexed target")
 
     VAL_TARGETS_CONVENTIONAL = [[target] for target in VAL_TARGETS]
     VAL_INDEXED_TARGET = TARGET_INDEXER.convert_samples_to_indexes(VAL_TARGETS_CONVENTIONAL)
+    VAL_INDEXED_TARGET = list(itertools.chain(*VAL_INDEXED_TARGET))
 
     logging.debug("Create validation indexed target")
 
     TEST_TARGETS_CONVENTIONAL = [[target] for target in TEST_TARGETS]
     TEST_INDEXED_TARGET = TARGET_INDEXER.convert_samples_to_indexes(TEST_TARGETS_CONVENTIONAL)
+    TEST_INDEXED_TARGET = list(itertools.chain(*TEST_INDEXED_TARGET))
 
     logging.debug("Create test indexed target")
 
     # create LM Tokenizer instance
-    TOKENIZER = BertTokenizer.from_pretrained(CONFIG.language_model_tokenizer_path)
-    MODEL = BertModel.from_pretrained(CONFIG.language_model_path, return_dict=True)
-    MODEL.eval()
+    # TOKENIZER = BertTokenizer.from_pretrained(CONFIG.language_model_tokenizer_path)
+    # MODEL = BertModel.from_pretrained(CONFIG.language_model_path, return_dict=True)
+    # MODEL.eval()
 
-    TRAIN_USER_EMBEDDINGS, TRAIN_USER_LABEL = create_user_embedding(TRAIN_DATA, MODEL, TOKENIZER)
+    MODEL = SentenceTransformer(CONFIG.sentence_transformers_path, device="cuda:0")
+
+    TRAIN_USER_EMBEDDINGS, TRAIN_USER_LABEL = create_user_embedding_sbert(TRAIN_DATA, MODEL)  # , TOKENIZER)
     logging.debug("Create train user embeddings")
 
-    VAL_USER_EMBEDDINGS, VAL_USER_LABEL = create_user_embedding(VAL_DATA, MODEL, TOKENIZER)
+    VAL_USER_EMBEDDINGS, VAL_USER_LABEL = create_user_embedding_sbert(VAL_DATA, MODEL)  # , TOKENIZER)
     logging.debug("Create validation user embeddings")
 
-    TEST_USER_EMBEDDINGS, TEST_USER_LABEL = create_user_embedding(TEST_DATA, MODEL, TOKENIZER)
+    TEST_USER_EMBEDDINGS, TEST_USER_LABEL = create_user_embedding_sbert(TEST_DATA, MODEL)  # , TOKENIZER)
     logging.debug("Create test user embeddings")
 
     # ----------------------------- Train SVM -----------------------------
@@ -93,14 +101,14 @@ if __name__ == "__main__":
     VAL_F1SCORE_MACRO = f1_score(VAL_INDEXED_TARGET, VAL_PREDICTED_TARGETS, average="macro")
     TEST_F1SCORE_MACRO = f1_score(TEST_INDEXED_TARGET, TEST_PREDICTED_TARGETS, average="macro")
 
-    logging.debug(f"Train macro F1 score is : {TRAIN_F1SCORE_MACRO*100:0.2f}")
-    logging.debug(f"Val macro F1 score is : {VAL_F1SCORE_MACRO*100:0.2f}")
-    logging.debug(f"Test macro F1 score is : {TEST_F1SCORE_MACRO*100:0.2f}")
+    logging.debug(f"Train macro F1 score is : {TRAIN_F1SCORE_MACRO * 100:0.2f}")
+    logging.debug(f"Val macro F1 score is : {VAL_F1SCORE_MACRO * 100:0.2f}")
+    logging.debug(f"Test macro F1 score is : {TEST_F1SCORE_MACRO * 100:0.2f}")
 
     TRAIN_F1SCORE_MICRO = f1_score(TRAIN_INDEXED_TARGET, TRAIN_PREDICTED_TARGETS, average="micro")
     VAL_F1SCORE_MICRO = f1_score(VAL_INDEXED_TARGET, VAL_PREDICTED_TARGETS, average="micro")
     TEST_F1SCORE_MICRO = f1_score(TEST_INDEXED_TARGET, TEST_PREDICTED_TARGETS, average="micro")
 
-    logging.debug(f"Train micro F1 score is : {TRAIN_F1SCORE_MICRO*100:0.2f}")
-    logging.debug(f"Val micro F1 score is : {VAL_F1SCORE_MICRO*100:0.2f}")
-    logging.debug(f"Test micro F1 score is : {TEST_F1SCORE_MICRO*100:0.2f}")
+    logging.debug(f"Train micro F1 score is : {TRAIN_F1SCORE_MICRO * 100:0.2f}")
+    logging.debug(f"Val micro F1 score is : {VAL_F1SCORE_MICRO * 100:0.2f}")
+    logging.debug(f"Test micro F1 score is : {TEST_F1SCORE_MICRO * 100:0.2f}")
